3aaac0c09cd6b47fcd426cfbbd8910eaa2db8f46
Okello David <grbsderrick@gmail.com>
2025-01-17 11:50:15 +0300

    update python

---
+++ name.py
@@ -0,0 +1,19 @@
+print("ENTER YOUR NAME BELOW")
+name = ["david", "bro", "freda"]
+
+firstName = name[0]
+firstLetterFirstName = firstName[0]
+
+print(firstName)
+print(f"the first letter in the name is {firstLetterFirstName}")
+
+s = set()
+s.add(9)
+s.add(1)
+s.add(3)
+
+
+s.sort(s)
+print(s)
+
+ 
